90e78c86f3447ed281eb22c80d27d17dbe25cbc1
Mahendra <160317468+mahendra189@users.noreply.github.com>
2024-08-23 12:46:36 +0000

    temp data

---
+++ quo/app/constants/data.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        'id':123,
+        'name': 'quotation',
+        'des': 'initial Rates',
+        'columns': [
+            "Sr",
+            "Particular",
+            "Rate",
+        ]
+    },
+    {
+        'id':12,
+        'name': 'Bill',
+        'des': 'Final Bills',
+        'columns': [
+            "Sr",
+            "Particular",
+            "Width",
+            "Height",
+            "Qnty",
+            "Unit",
+            "Rate",
+            "Amount"
+        ]
+    }
+]
